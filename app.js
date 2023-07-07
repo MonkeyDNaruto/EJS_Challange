38eab9aa04d9d131a2d0f638cd90a72f1f80b935
NischalShrestha <nischalshrestha324@gmain.com>
2023-07-07 13:35:44 +0545

    challange 19 completed

--- app.js
+++ app.js
@@ -61,11 +61,6 @@ app.get("/posts/:postName", (req, res) => {
   res.redirect("/");
 });
 
-
-
-
-
-
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(3000, (req, res) => {
+  console.log("Server is running on port: 3000");
 });
